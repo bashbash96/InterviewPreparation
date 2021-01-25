400bbfc25abf789cc2b249b8a2e7c42eceab86f9
bashbash96 <amjad.bashiti.96@gmail.com>
2021-01-25 12:22:20 +0200

    Hard question

--- LeetCode/hard.py
+++ LeetCode/hard.py
@@ -1,4 +1,67 @@
 # -----------------------------------------------------------------------
+"""
+42. Trapping Rain Water
+Given n non-negative integers representing an elevation map where the width of each bar is 1,
+compute how much water it can trap after raining.
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
+In this case, 6 units of rain water (blue section) are being trapped.
+"""
+
+
+class Solution:
+
+    #     def trap(self, height):
+    #         if len(height) == 0:
+    #             return 0
+    #         n = len(height)
+    #         leftMax, rightMax = [0] * n , [0] * n
+    #         i, j = 0, len(height) - 1
+    #         currLeftMax = height[0]
+    #         currRightMax = height[len(height) - 1]
+    #         while i < len(height):
+    #             if height[i] > currLeftMax:
+    #                 currLeftMax = height[i]
+
+    #             if height[j] > currRightMax:
+    #                 currRightMax = height[j]
+
+    #             leftMax[i] = currLeftMax
+    #             rightMax[j] = currRightMax
+    #             i += 1
+    #             j -= 1
+    #         ans = 0
+    #         for i in range(n):
+    #             ans += (min(leftMax[i], rightMax[i]) - height[i])
+
+    #         return ans
+
+    #         # time O(n)
+    #         # space O(n)
+
+    def trap(self, height):
+
+        left, right, lMax, rMax = 0, len(height) - 1, 0, 0
+        ans = 0
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= lMax:
+                    lMax = height[left]
+                else:
+                    ans += lMax - height[left]
+                left += 1
+            else:
+                if height[right] >= rMax:
+                    rMax = height[right]
+                else:
+                    ans += rMax - height[right]
+                right -= 1
+
+        return ans
+
+        # time O(n)
+        # space O(1)
 
 # -----------------------------------------------------------------------
 
