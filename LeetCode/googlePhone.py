a2add801dd394ac01aaddf3f935b9bd211da50fb
bashbash96 <amjad.bashiti.96@gmail.com>
2021-01-25 17:32:29 +0200

    Google phone questions

--- LeetCode/googlePhone.py
+++ LeetCode/googlePhone.py
@@ -666,7 +666,6 @@ All the dimensions must be strictly smaller to fit into a larger box -- [1,5,3] 
 
 def numOfBoxes(dims):
     dims.sort(key=lambda x: (x[0], x[1], x[2]))
-    print(dims)
     memo = [0 for i in range(len(dims))]
     res = 0
     for i in range(len(dims)):
@@ -684,9 +683,66 @@ def numOfBoxes(dims):
 
 # -----------------------------------------------------------------------
 """
+8. String to Integer (atoi)
 
+Implement atoi which converts a string to an integer.
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. 
+Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits 
+as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no 
+effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists 
+because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned.
+
+Note:
+
+Only the space character ' ' is considered as whitespace character.
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: 
+[−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
 """
+
+
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        res = 0
+        if len(str) == 0:
+            return res
+        neg = False
+        i = 0
+        while i < len(str) and str[i] == ' ':
+            i += 1
+
+        if i < len(str):
+            if str[i] == '-':
+                neg = True
+                i += 1
+            elif str[i] == '+':
+                i += 1
+            elif str[i] > '9' or str[i] < '0':
+                return res
+        while i < len(str) and str[i] >= '0' and str[i] <= '9':
+            res = res * 10 + int(str[i])
+            i += 1
+
+        res = res if not neg else res * -1
+
+        if res < (-1 * (2 ** 31)):
+            return -1 * (2 ** 31)
+        if res > (2 ** 31) - 1:
+            return (2 ** 31) - 1
+
+        return res
+
+        # time O(n)
+        # space O(n)
+
 # -----------------------------------------------------------------------
+
 
 # -----------------------------------------------------------------------
 
