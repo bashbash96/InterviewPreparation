0bf984acee65acacc947e20458a72ba56da2ae22
bashbash96 <amjad.bashiti.96@gmail.com>
2021-01-27 17:52:37 +0200

    Easy questions

--- LeetCode/easy.py
+++ LeetCode/easy.py
@@ -159,13 +159,151 @@ class Solution:
     # time O(n)
     # space O(1)
 
-# -----------------------------------------------------------------------
-"""
-
-"""
-# -----------------------------------------------------------------------
 
 # -----------------------------------------------------------------------
+"""
+283. Move Zeroes
+
+Given an array nums, write a function to move all 0's to the end of it while maintaining the 
+relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations
+"""
+
+
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        firstZero, firstNonZero = 0, 0
+
+        while firstZero < len(nums) and firstNonZero < len(nums):
+
+            while firstZero < len(nums) and nums[firstZero] != 0:
+                firstZero += 1
+
+            while firstNonZero < len(nums) and nums[firstNonZero] == 0:
+                firstNonZero += 1
+
+            if firstZero < len(nums) and firstNonZero < len(nums):
+                if firstZero < firstNonZero:
+                    # swap them
+                    nums[firstZero], nums[firstNonZero] = nums[firstNonZero], nums[firstZero]
+                firstNonZero += 1
+
+        # time O(n)
+        # space O(1)
+
+
+# -----------------------------------------------------------------------
+"""
+448. Find All Numbers Disappeared in an Array
+
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[5,6]
+
+"""
+
+
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        if not nums:
+            return []
+
+        for i in range(len(nums)):
+            curr_index = abs(nums[i]) - 1
+            if nums[curr_index] < 0:
+                continue
+            nums[curr_index] *= -1
+
+        res = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                res.append(i + 1)
+
+        return res
+
+    # time O(n)
+    # space O(1)
+
+
+# -----------------------------------------------------------------------
+"""
+21. Merge Two Sorted Lists
+
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together 
+the nodes of the first two lists.
+
+ 
+Example 1:
+
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: l1 = [], l2 = []
+Output: []
+
+Example 3:
+
+Input: l1 = [], l2 = [0]
+Output: [0]
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        res = ListNode('DUMMY')
+        curr = res
+
+        p1, p2 = l1, l2
+
+        while p1 and p2:
+
+            if p1.val < p2.val:
+                curr.next = p1
+                p1 = p1.next
+            else:
+                curr.next = p2
+                p2 = p2.next
+
+            curr = curr.next
+
+        if p1:
+            curr.next = p1
+
+        if p2:
+            curr.next = p2
+
+        return res.next
+
+    # time O(n)
+    # space O(1)
 
 # -----------------------------------------------------------------------
 
