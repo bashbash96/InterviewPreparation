740d4882bf688bad18dd1e6e0b6154fa0b3ea654
bashbash96 <amjad.bashiti.96@gmail.com>
2021-02-17 23:07:29 +0200

    Medium questions

--- LeetCode/medium.py
+++ LeetCode/medium.py
@@ -1406,11 +1406,218 @@ class Solution:
     # time O(n)
     # space O(1)
 
-# -----------------------------------------------------------------------
 
 # -----------------------------------------------------------------------
+"""
+951. Flip Equivalent Binary Trees
+Medium
+
+918
+
+47
+
+Add to List
+
+Share
+For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+
+A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+
+Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivelent or false otherwise.
+
+ 
+
+Example 1:
+
+Flipped Trees Diagram
+Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+Output: true
+Explanation: We flipped at nodes with values 1, 3, and 5.
+Example 2:
+
+Input: root1 = [], root2 = []
+Output: true
+Example 3:
+
+Input: root1 = [], root2 = [1]
+Output: false
+Example 4:
+
+Input: root1 = [0,null,1], root2 = []
+Output: false
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def flipEquiv(self, root1, root2):
+
+        if not root1 and not root2:
+            return True
+
+        if not root1 or not root2 or root1.val != root2.val:
+            return False
+
+        return (self.flipEquiv(root1.left, root2.left) or self.flipEquiv(root1.left, root2.right)) and (
+                self.flipEquiv(root1.right, root2.left) or self.flipEquiv(root1.right, root2.right))
+
+    # time O(n + m)
+    # space O(h)
+
 
 # -----------------------------------------------------------------------
+"""
+1219. Path with Maximum Gold
+
+In a gold mine grid of size m * n, each cell in this mine has an integer representing the amount of gold in that cell, 0 if it is empty.
+
+Return the maximum amount of gold you can collect under the conditions:
+
+Every time you are located in a cell you will collect all the gold in that cell.
+From your position you can walk one step to the left, right, up or down.
+You can't visit the same cell more than once.
+Never visit a cell with 0 gold.
+You can start and stop collecting gold from any position in the grid that has some gold.
+ 
+
+Example 1:
+
+Input: grid = [[0,6,0],[5,8,7],[0,9,0]]
+Output: 24
+Explanation:
+[[0,6,0],
+ [5,8,7],
+ [0,9,0]]
+Path to get the maximum gold, 9 -> 8 -> 7.
+Example 2:
+
+Input: grid = [[1,0,7],[2,0,6],[3,4,5],[0,3,0],[9,0,20]]
+Output: 28
+Explanation:
+[[1,0,7],
+ [2,0,6],
+ [3,4,5],
+ [0,3,0],
+ [9,0,20]]
+Path to get the maximum gold, 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7.
+ 
+
+Constraints:
+
+1 <= grid.length, grid[i].length <= 15
+0 <= grid[i][j] <= 100
+There are at most 25 cells containing gold.
+"""
+
+
+class Solution:
+    def getMaximumGold(self, grid):
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                dfs_visit(grid, row, col)
+
+        global max_gold
+        return max_gold
+
+    # time O(n * m)
+    # space O(1) since the longets path is 25
+
+
+def is_valid(grid, row, col):
+    if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]) or grid[row][col] == 0:
+        return False
+
+    return True
+
+
+max_gold = 0
+curr_gold = 0
+
+
+def dfs_visit(grid, row, col):
+    if not is_valid(grid, row, col):
+        return
+
+    global curr_gold
+    cell_gold = grid[row][col]
+    curr_gold += cell_gold
+    grid[row][col] = 0
+
+    global max_gold
+    max_gold = max(max_gold, curr_gold)
+
+    for n_row, n_col in get_neighbors(row, col):
+        dfs_visit(grid, n_row, n_col)
+
+    curr_gold -= cell_gold
+    grid[row][col] = cell_gold
+
+
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+
+def get_neighbors(row, col):
+    global directions
+    neighbors = []
+
+    for dx, dy in directions:
+        neighbors.append((row + dx, col + dy))
+
+    return neighbors
+
+
+# -----------------------------------------------------------------------
+"""
+159. Longest Substring with At Most Two Distinct Characters
+
+Given a string s , find the length of the longest substring t  that contains at most 2 distinct characters.
+
+Example 1:
+
+Input: "eceba"
+Output: 3
+Explanation: t is "ece" which its length is 3.
+Example 2:
+
+Input: "ccaabbb"
+Output: 5
+Explanation: t is "aabbb" which its length is 5.
+"""
+
+from collections import defaultdict
+
+
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        counts = defaultdict(int)
+        left = 0
+
+        start_idx, length = 0, 0
+        for i, char in enumerate(s):
+            counts[char] += 1
+            if len(counts) <= 2:
+                if i - left + 1 > length:
+                    length = i - left + 1
+                    start_idx = left
+            else:
+                while left < i and len(counts) > 2:
+                    counts[s[left]] -= 1
+                    if counts[s[left]] == 0:
+                        del counts[s[left]]
+                    left += 1
+
+        return length
+
+    # time O(n)
+    # space O(1)
 
 # -----------------------------------------------------------------------
 
