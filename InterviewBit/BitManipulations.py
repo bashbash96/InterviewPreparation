4bff773b7a70f9db7cd54577020c5b97acb56268
bashbash96 <amjad.bashiti.96@gmail.com>
2021-02-21 16:26:46 +0200

    Bit manipulations questions

--- InterviewBit/BitManipulations.py
+++ InterviewBit/BitManipulations.py
@@ -66,7 +66,55 @@ class Solution:
     # time O(n)
     # space O(1)
 
+
 # -----------------------------------------------------------------------
+"""
+Different Bits Sum Pairwise
+
+We define f(X, Y) as number of different corresponding bits in binary representation of X and Y. For example, f(2, 7) = 2, since binary representation of 2 and 7 are 010 and 111, respectively. The first and the third bit differ, so f(2, 7) = 2.
+
+You are given an array of N positive integers, A1, A2 ,…, AN. Find sum of f(Ai, Aj) for all pairs (i, j) such that 1 ≤ i, j ≤ N. Return the answer modulo 109+7.
+
+For example,
+
+A=[1, 3, 5]
+
+We return
+
+f(1, 1) + f(1, 3) + f(1, 5) + 
+f(3, 1) + f(3, 3) + f(3, 5) +
+f(5, 1) + f(5, 3) + f(5, 5) =
+
+0 + 1 + 1 +
+1 + 0 + 2 +
+1 + 2 + 0 = 8
+"""
+
+BITS_NUM = 32
+MOD = pow(10, 9) + 7
+
+
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def cntBits(self, A):
+        total_count = 0
+
+        for i in range(BITS_NUM):
+            count_ones, count_zeros = 0, 0
+            for num in A:
+                num >>= i
+                if num & 1:
+                    count_ones += 1
+                else:
+                    count_zeros += 1
+
+            total_count += (count_ones * count_zeros * 2) % MOD
+
+        return total_count % MOD
+
+    # time O(n)
+    # space O(1)
 
 # -----------------------------------------------------------------------
 # -----------------------------------------------------------------------
