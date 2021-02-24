d444e0b98f3791a84918a59d7e934062d2c52093
bashbash96 <amjad.bashiti.96@gmail.com>
2021-02-24 20:10:46 +0200

    Medium questions

--- LeetCode/medium.py
+++ LeetCode/medium.py
@@ -2620,6 +2620,75 @@ class Solution(object):
     # space O(n)
 
 # -----------------------------------------------------------------------
+"""
+652. Find Duplicate Subtrees
+
+Given the root of a binary tree, return all duplicate subtrees.
+
+For each kind of duplicate subtrees, you only need to return the root node of any one of them.
+
+Two trees are duplicate if they have the same structure with the same node values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,null,2,4,null,null,4]
+Output: [[2,4],[4]]
+Example 2:
+
+
+Input: root = [2,1,1]
+Output: [[1]]
+Example 3:
+
+
+Input: root = [2,2,2,3,null,3,null]
+Output: [[2,3],[3]]
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import defaultdict
+
+
+class Solution(object):
+    def findDuplicateSubtrees(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[TreeNode]
+        """
+        count = defaultdict(int)
+        res = []
+
+        count_duplicates(root, res, count)
+
+        return res
+
+    # time O(n)
+    # space O(n^2)
+
+
+def count_duplicates(root, res, count):
+    if not root:
+        return '#'
+
+    left = count_duplicates(root.left, res, count)
+    right = count_duplicates(root.right, res, count)
+    curr_path = "{},{},{}".format(root.val, left, right)
+
+    count[curr_path] += 1
+    if count[curr_path] == 2:
+        res.append(root)
+
+    return curr_path
+
 
 # -----------------------------------------------------------------------
 
