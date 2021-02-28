e2421b7a4f9f169fa9dab2b417711bb3d76885f3
bashbash96 <amjad.bashiti.96@gmail.com>
2021-02-28 18:55:53 +0200

    Easy questions

--- LeetCode/easy.py
+++ LeetCode/easy.py
@@ -2001,10 +2001,77 @@ class Solution(object):
     # time O(n)
     # space O(n)
 
-# -----------------------------------------------------------------------
 
 # -----------------------------------------------------------------------
+"""
+258. Add Digits
 
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Example:
+
+Input: 38
+Output: 2 
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+             Since 2 has only one digit, return it.
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+"""
+
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+
+        curr_num = str(num)
+
+        while len(curr_num) > 1:
+            next_num = sum([int(dig) for dig in curr_num])
+            curr_num = str(next_num)
+
+        return curr_num
+
+    # time O(log(n))
+    # space O(1)
+
+
+# -----------------------------------------------------------------------
+"""
+9. Palindrome Number
+
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+"""
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        return reverse_num(x) == x
+
+
+def reverse_num(num):
+    rev = 0
+    while num > 0:
+        rev = rev * 10 + num % 10
+        num //= 10
+
+    return rev
 # -----------------------------------------------------------------------
 
 # -----------------------------------------------------------------------
