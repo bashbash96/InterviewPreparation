4a3509aa332edfb214c2176d7615f96b22eddaf0
bashbash96 <amjad.bashiti.96@gmail.com>
2021-03-03 21:24:38 +0200

    Puzzels questions

--- InterviewBit/Puzzels.py
+++ InterviewBit/Puzzels.py
@@ -15,7 +15,46 @@ How old are the daughters?
 # 3 3 8
 
 # -----------------------------------------------------------------------
+"""
+Cross the Bridge
+
+Four people are on this side of the bridge. Everyone has to get across.
+Problem is that it’s dark and so you can’t cross the bridge without a flashlight and they only have one flashlight.
+Plus the bridge is only big enough for two people to cross at once.
+The four people walk at different speeds:
+
+one fella is so fast it only takes him 1 minute to cross the bridge,
+another 2 minutes,
+a third 5 minutes,
+the last it takes 10 minutes to cross the bridge.
+When two people cross the bridge together (sharing the flashlight), they both walk at the slower person’s pace. 
+What is the minimum time required for all 4 to cross the bridge.
+
+Respond with a number which represents the number of minutes.
+
+ Answer is a integer. Just put the number without any decimal places if its an integer. 
+ If the answer is Infinity, output Infinity. Feel free to get in touch with us if you have any questions 
+"""
+
+# 17
+# 1 and 2 cross, 1 return -> 2 + 1
+# 5 and 10 cross, 2 return -> 10 + 2
+# 1 and 2 cross -> 2
+# overall 17
+
 # -----------------------------------------------------------------------
+"""
+Divide the Cake
+
+Consider a rectangular cake with a rectangular section (of any size or orientation) removed from it. 
+Is it possible to divide the cake exactly in half with only one cut?
+"""
+
+# Only when the rectangular section passes through the centre
+# Only if the centre of both rectangles match
+# Only when rectangular section has axis parallel to the actual rectangle
+# Always possible <-- cut it horizontally
+
 # -----------------------------------------------------------------------
 # -----------------------------------------------------------------------
 # -----------------------------------------------------------------------
