6203fa841d932934b56ab0cf510954cced943c4a
bashbash96 <amjad.bashiti.96@gmail.com>
2021-03-08 21:18:16 +0200

    Easy questions

--- LeetCode/easy.py
+++ LeetCode/easy.py
@@ -3134,6 +3134,53 @@ class Solution(object):
     # space O(n)
 
 # -----------------------------------------------------------------------
+"""
+836. Rectangle Overlap
+
+An axis-aligned rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) is the coordinate of its bottom-left corner, and (x2, y2) is the coordinate of its top-right corner. Its top and bottom edges are parallel to the X-axis, and its left and right edges are parallel to the Y-axis.
+
+Two rectangles overlap if the area of their intersection is positive. To be clear, two rectangles that only touch at the corner or edges do not overlap.
+
+Given two axis-aligned rectangles rec1 and rec2, return true if they overlap, otherwise return false.
+
+ 
+
+Example 1:
+
+Input: rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+Output: true
+"""
+
+
+class Solution(object):
+    def isRectangleOverlap(self, rec1, rec2):
+        """
+        :type rec1: List[int]
+        :type rec2: List[int]
+        :rtype: bool
+        """
+
+        p1 = (rec1[0], rec1[1])
+        p2 = (rec1[2], rec1[3])
+
+        p3 = (rec2[0], rec2[1])
+        p4 = (rec2[2], rec2[3])
+
+        # the same line
+        if p1[0] == p2[0] or p1[1] == p2[1] or p3[0] == p4[0] or p3[1] == p4[1]:
+            return False
+
+        left = (p2[0] <= p3[0])
+        right = (p1[0] >= p4[0])
+        bottom = (p2[1] <= p3[1])
+        top = (p1[1] >= p4[1])
+
+        return not (left or right or bottom or top)
+
+    # time O(1)
+    # space O(1)
+
+
 # -----------------------------------------------------------------------
 # -----------------------------------------------------------------------
 
