b82d03504e6da6042475f8d4ccaa803993b88082
bashbash96 <amjad.bashiti.96@gmail.com>
2021-03-10 19:44:32 +0200

    Easy questions

--- LeetCode/easy.py
+++ LeetCode/easy.py
@@ -361,6 +361,70 @@ class Solution:
 # -----------------------------------------------------------------------
 
 """
+122. Best Time to Buy and Sell Stock II
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e., max profit = 0.
+"""
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        n = len(prices)
+
+        max_profit = 0
+
+        min_idx, max_idx = 0, 0
+
+        while min_idx < n - 1 and max_idx < n - 1:
+            while min_idx < n - 1 and prices[min_idx] > prices[min_idx + 1]:
+                min_idx += 1
+
+            max_idx = min_idx + 1
+            while max_idx < n - 1 and prices[max_idx] < prices[max_idx + 1]:
+                max_idx += 1
+
+            if max_idx < n:
+                max_profit += prices[max_idx] - prices[min_idx]
+                min_idx = max_idx
+
+        return max_profit
+
+    # time O(n)
+    # space O(1)
+
+
+# -----------------------------------------------------------------------
+
+
+"""
 543. Diameter of Binary Tree
 
 Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree 
@@ -3133,6 +3197,7 @@ class Solution(object):
     # time O(n)
     # space O(n)
 
+
 # -----------------------------------------------------------------------
 """
 836. Rectangle Overlap
@@ -3179,7 +3244,6 @@ class Solution(object):
 
     # time O(1)
     # space O(1)
-
 
 # -----------------------------------------------------------------------
 # -----------------------------------------------------------------------
