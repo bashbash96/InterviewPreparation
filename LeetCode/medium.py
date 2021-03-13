da8b45ba453b8d59de3263e70393645aea9cc67e
bashbash96 <amjad.bashiti.96@gmail.com>
2021-03-13 23:08:53 +0200

    Medium questions

--- LeetCode/medium.py
+++ LeetCode/medium.py
@@ -5668,7 +5668,51 @@ class Solution(object):
     # time O(n)
     # space O(1)
 
+
 # -----------------------------------------------------------------------
+"""
+939. Minimum Area Rectangle
+
+Given a set of points in the xy-plane, determine the minimum area of a rectangle formed from these points, with sides parallel to the x and y axes.
+
+If there isn't any rectangle, return 0.
+
+ 
+
+Example 1:
+
+Input: [[1,1],[1,3],[3,1],[3,3],[2,2]]
+Output: 4
+Example 2:
+
+Input: [[1,1],[1,3],[3,1],[3,3],[4,1],[4,3]]
+Output: 2
+"""
+
+
+class Solution(object):
+    def minAreaRect(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+
+        seen = set()
+        min_area = float('inf')
+
+        for x1, y1 in points:
+
+            for x2, y2 in seen:
+                if (x1, y2) in seen and (x2, y1) in seen:
+                    area = abs(x1 - x2) * abs(y1 - y2)
+                    min_area = min(min_area, area)
+
+            seen.add((x1, y1))
+
+        return min_area if min_area != float('inf') else 0
+
+    # time O(n^2)
+    # space O(n)
 
 # -----------------------------------------------------------------------
 
