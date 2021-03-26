3c78604e5a11cbe00e58b58f3da97d2a76794492
bashbash96 <amjad.bashiti.96@gmail.com>
2021-03-26 19:52:52 +0300

    Greedy questions

--- InterviewBit/Greedy.py
+++ InterviewBit/Greedy.py
@@ -68,7 +68,106 @@ class Solution:
     # time O(n)
     # space O(1)
 
+
 # -----------------------------------------------------------------------
+"""
+Disjoint Intervals
+
+Problem Description
+
+Given a set of N intervals denoted by 2D array A of size N x 2, the task is to find the length of maximal set of mutually disjoint intervals.
+
+Two intervals [x, y] & [p, q] are said to be disjoint if they do not have any point in common.
+
+Return a integer denoting the length of maximal set of mutually disjoint intervals.
+
+
+
+Problem Constraints
+1 <= N <= 105
+
+1 <= A[i][0] <= A[i][1] <= 109
+
+
+
+Input Format
+First and only argument is a 2D array A of size N x 2 denoting the N intervals.
+
+
+
+Output Format
+Return a integer denoting the length of maximal set of mutually disjoint intervals.
+
+
+
+Example Input
+Input 1:
+
+ A = [
+       [1, 4]
+       [2, 3]
+       [4, 6]
+       [8, 9]
+     ]
+Input 2:
+
+ A = [
+       [1, 9]
+       [2, 3]
+       [5, 7]
+     ]
+
+
+Example Output
+Output 1:
+
+ 3
+Output 2:
+
+ 2
+
+
+Example Explanation
+Explanation 1:
+
+ Intervals: [ [1, 4], [2, 3], [4, 6], [8, 9] ]
+ Intervals [2, 3] and [1, 4] overlap.
+ We must include [2, 3] because if [1, 4] is included thenwe cannot include [4, 6].
+ We can include at max three disjoint intervals: [[2, 3], [4, 6], [8, 9]]
+Explanation 2:
+
+ Intervals: [ [1, 9], [2, 3], [5, 7] ]
+ We can include at max two disjoint intervals: [[2, 3], [5, 7]]
+"""
+
+
+class Solution:
+    # @param A : list of list of integers
+    # @return an integer
+    def solve(self, A):
+
+        if len(A) == 0:
+            return 0
+
+        A.sort(key=lambda x: x[1])
+
+        curr = A[0]
+        res = 1
+
+        for i in range(1, len(A)):
+            if is_disjoint(curr, A[i]):
+                curr = A[i]
+                res += 1
+
+        return res
+
+    # time O(n)
+    # space O(1)
+
+
+def is_disjoint(inter1, inter2):
+    return inter2[0] > inter1[1]
+
 # -----------------------------------------------------------------------
 # -----------------------------------------------------------------------
 # -----------------------------------------------------------------------
