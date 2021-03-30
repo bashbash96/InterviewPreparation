c72b0a73779b44220cb757cdbaa956ea39a2a7db
bashbash96 <amjad.bashiti.96@gmail.com>
2021-03-30 21:34:08 +0300

    Hard questions

--- LeetCode/hard.py
+++ LeetCode/hard.py
@@ -3048,7 +3048,81 @@ class Solution(object):
     # time O(n * log(n))
     # space O(n)
 
+
 # -----------------------------------------------------------------------
+"""
+1776. Car Fleet II
+
+There are n cars traveling at different speeds in the same direction along a one-lane road. You are given an array cars of length n, where cars[i] = [positioni, speedi] represents:
+
+positioni is the distance between the ith car and the beginning of the road in meters. It is guaranteed that positioni < positioni+1.
+speedi is the initial speed of the ith car in meters per second.
+For simplicity, cars can be considered as points moving along the number line. Two cars collide when they occupy the same position. Once a car collides with another car, they unite and form a single car fleet. The cars in the formed fleet will have the same position and the same speed, which is the initial speed of the slowest car in the fleet.
+
+Return an array answer, where answer[i] is the time, in seconds, at which the ith car collides with the next car, or -1 if the car does not collide with the next car. Answers within 10-5 of the actual answers are accepted.
+
+ 
+
+Example 1:
+
+Input: cars = [[1,2],[2,1],[4,3],[7,2]]
+Output: [1.00000,-1.00000,3.00000,-1.00000]
+Explanation: After exactly one second, the first car will collide with the second car, and form a car fleet with speed 1 m/s. After exactly 3 seconds, the third car will collide with the fourth car, and form a car fleet with speed 2 m/s.
+Example 2:
+
+Input: cars = [[3,4],[5,4],[6,3],[9,1]]
+Output: [2.00000,1.00000,1.50000,-1.00000]
+"""
+
+
+class Solution(object):
+    def getCollisionTimes(self, cars):
+        """
+        :type cars: List[List[int]]
+        :rtype: List[float]
+        """
+        if len(cars) == 0:
+            return []
+
+        n = len(cars)
+        times = [-1] * n
+        stack = [n - 1]
+
+        for i in range(n - 2, -1, -1):
+
+            curr_p, curr_s = cars[i]
+
+            while stack:
+                last_p, last_s = cars[stack[-1]]
+                last_idx = stack[-1]
+                if last_s >= curr_s:
+                    stack.pop()
+                elif times[last_idx] >= 0 and calc_time(cars[i], cars[last_idx]) >= times[last_idx]:
+                    stack.pop()
+                else:
+                    break
+
+            if stack:
+                times[i] = calc_time(cars[i], cars[stack[-1]])
+
+            stack.append(i)
+
+        return times
+
+    # time O(n)
+    # space O(n)
+
+
+def calc_time(car1, car2):
+    """
+    s = d / t => t = d / s
+    t = delta d / delta s
+    """
+
+    distance_diff = car2[0] - car1[0]
+    speed_diff = car1[1] - car2[1]
+
+    return float(distance_diff) / speed_diff
 
 # -----------------------------------------------------------------------
 # -----------------------------------------------------------------------
