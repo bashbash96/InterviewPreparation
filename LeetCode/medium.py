db56b4a76e351fc818079e8f5455e8ffeb2cf335
bashbash96 <amjad.bashiti.96@gmail.com>
2021-04-02 23:18:26 +0300

    Medium questions

--- LeetCode/medium.py
+++ LeetCode/medium.py
@@ -7552,7 +7552,83 @@ class Solution(object):
     # time O(n)
     # space O(n)
 
+
 # -----------------------------------------------------------------------
+"""
+1605. Find Valid Matrix Given Row and Column Sums
+
+You are given two arrays rowSum and colSum of non-negative integers where rowSum[i] is the sum of the elements in the ith row and colSum[j] is the sum of the elements of the jth column of a 2D matrix. In other words, you do not know the elements of the matrix, but you do know the sums of each row and column.
+
+Find any matrix of non-negative integers of size rowSum.length x colSum.length that satisfies the rowSum and colSum requirements.
+
+Return a 2D array representing any matrix that fulfills the requirements. It's guaranteed that at least one matrix that fulfills the requirements exists.
+
+ 
+
+Example 1:
+
+Input: rowSum = [3,8], colSum = [4,7]
+Output: [[3,0],
+         [1,7]]
+Explanation:
+0th row: 3 + 0 = 3 == rowSum[0]
+1st row: 1 + 7 = 8 == rowSum[1]
+0th column: 3 + 1 = 4 == colSum[0]
+1st column: 0 + 7 = 7 == colSum[1]
+The row and column sums match, and all matrix elements are non-negative.
+Another possible matrix is: [[1,2],
+                             [3,5]]
+Example 2:
+
+Input: rowSum = [5,7,10], colSum = [8,6,8]
+Output: [[0,5,0],
+         [6,1,0],
+         [2,0,8]]
+Example 3:
+
+Input: rowSum = [14,9], colSum = [6,9,8]
+Output: [[0,9,5],
+         [6,0,3]]
+Example 4:
+
+Input: rowSum = [1,0], colSum = [1]
+Output: [[1],
+         [0]]
+Example 5:
+
+Input: rowSum = [0], colSum = [0]
+Output: [[0]]
+"""
+
+
+class Solution(object):
+    def restoreMatrix(self, rowSum, colSum):
+        """
+        :type rowSum: List[int]
+        :type colSum: List[int]
+        :rtype: List[List[int]]
+        """
+
+        n = len(rowSum)
+        m = len(colSum)
+
+        result = [[0 for _ in range(m)] for _ in range(n)]
+
+        i = 0
+        j = 0
+        while i < n and j < m:
+            result[i][j] = min(rowSum[i], colSum[j])
+            rowSum[i] -= result[i][j]
+            colSum[j] -= result[i][j]
+            if rowSum[i] == 0:
+                i += 1
+            if colSum[j] == 0:
+                j += 1
+
+        return result
+
+    # time O(n + m)
+    # space O(n*m)
 
 # -----------------------------------------------------------------------
 
