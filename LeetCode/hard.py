20511f9268a172f641311b860eb881ca19e9abcf
bashbash96 <amjad.bashiti.96@gmail.com>
2021-04-05 21:23:31 +0300

    Hard questions

--- LeetCode/hard.py
+++ LeetCode/hard.py
@@ -3921,7 +3921,86 @@ def is_valid(grid, point):
 
     return True
 
+
 # -----------------------------------------------------------------------
+"""
+10. Regular Expression Matching
+
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*' where: 
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+ 
+
+Example 1:
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+Example 3:
+
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+Example 4:
+
+Input: s = "aab", p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+Example 5:
+
+Input: s = "mississippi", p = "mis*is*p*."
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 20
+0 <= p.length <= 30
+s contains only lowercase English letters.
+p contains only lowercase English letters, '.', and '*'.
+It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+"""
+
+
+class Solution(object):
+    def isMatch(self, text, pattern):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+
+        return is_match(text, pattern, 0, 0, {})
+
+    # time O(n * m)
+    # space O(n * m)
+
+
+def is_match(s, p, p1, p2, memo):
+    if (p1, p2) in memo:
+        return memo[(p1, p2)]
+
+    if p2 == len(p):
+        memo[(p1, p2)] = p1 == len(s)
+        return memo[(p1, p2)]
+
+    match = p1 < len(s) and p[p2] in [s[p1], '.']
+    res = False
+    if p2 + 1 < len(p) and p[p2 + 1] == '*':
+        res = is_match(s, p, p1, p2 + 2, memo) or (match and is_match(s, p, p1 + 1, p2, memo))
+    else:
+        res = match and is_match(s, p, p1 + 1, p2 + 1, memo)
+
+    memo[(p1, p2)] = res
+    return res
 
 # -----------------------------------------------------------------------
 
