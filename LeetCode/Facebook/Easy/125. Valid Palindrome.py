f4dea39b3fe98fb43ca83614bdeb543a6ca9b86e
bashbash96 <amjad.bashiti.96@gmail.com>
2021-07-10 18:46:28 +0300

    Easy questions

---
+++ LeetCode/Facebook/Easy/125. Valid Palindrome.py
@@ -0,0 +1,53 @@
+"""
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+"""
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        left, right = 0, len(s) - 1
+
+        while left < right:
+
+            if not s[left].isalnum():
+                left += 1
+                continue
+
+            if not s[right].isalnum():
+                right -= 1
+                continue
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+    # time O(n)
+    # space O(1)
