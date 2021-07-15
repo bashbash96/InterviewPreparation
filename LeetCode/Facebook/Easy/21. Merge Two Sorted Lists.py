ef5ceeacc83e9dbdefe4c2c568e8cdc64e562802
bashbash96 <amjad.bashiti.96@gmail.com>
2021-07-15 19:23:39 +0300

    Easy questions

---
+++ LeetCode/Facebook/Easy/21. Merge Two Sorted Lists.py
@@ -0,0 +1,63 @@
+"""
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+
+
+Example 1:
+
+
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: l1 = [], l2 = []
+Output: []
+Example 3:
+
+Input: l1 = [], l2 = [0]
+Output: [0]
+
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both l1 and l2 are sorted in non-decreasing order.
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+        res = ListNode('DUMMY')
+
+        curr = res
+
+        while l1 or l2:
+
+            val1 = l1.val if l1 else float('inf')
+            val2 = l2.val if l2 else float('inf')
+
+            if val1 < val2:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+
+            curr = curr.next
+
+        return res.next
+
+    # time O(n)
+    # space O(1)
